abdbd226b2d225c8f48b30f0067cfc806c74a4d2
Mannulus <pipe_050401@hotmail.com>
2025-08-10 23:29:08 -0500

    feat: add 'Términos y condiciones' component with routing

---
+++ src/app/modules/hojadevida/terminos-condiciones/terminos-condiciones.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+
+@Component({
+  selector: 'app-terminos-condiciones',
+  imports: [MatDividerModule],
+  templateUrl: './terminos-condiciones.component.html',
+  styleUrl: './terminos-condiciones.component.scss',
+})
+export class TerminosCondicionesComponent { }
