8018495e050166c798aa78d53708ca44964ce00a
Mannulus <pipe_050401@hotmail.com>
2025-08-13 13:34:56 -0500

    feat: env for https service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   supabaseUrl: 'https://alrlskhovavhnvldejez.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFscmxza2hvdmF2aG52bGRlamV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTExNDM4MTcsImV4cCI6MjA2NjcxOTgxN30.zLISh3rdEV7Qst-xLhw8QxcKe3rbUTrYQKfU4UKhMCE',
   //hojaDeVidaApiUrl: 'http://localhost:8080/bipal/api',
-  hojaDeVidaApiUrl: 'http://santoservice.online:8080/bipal/api',
+  hojaDeVidaApiUrl: 'https://santoservice.online/bipal/api',
 }
