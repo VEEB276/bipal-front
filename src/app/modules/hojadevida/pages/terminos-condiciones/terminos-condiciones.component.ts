bd14468e6dd20f007be367c68387155021bdf8a0
Mannulus <pipe_050401@hotmail.com>
2025-08-24 19:59:25 -0500

    feat: link terms and add back button to terms and conditions

--- src/app/modules/hojadevida/pages/terminos-condiciones/terminos-condiciones.component.ts
+++ src/app/modules/hojadevida/pages/terminos-condiciones/terminos-condiciones.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-import { MatDividerModule } from '@angular/material/divider';
+import { Component, inject } from "@angular/core";
+import { MatButton } from "@angular/material/button";
+import { MatDividerModule } from "@angular/material/divider";
+import { ActivatedRoute, RouterLink } from "@angular/router";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
-  selector: 'app-terminos-condiciones',
-  imports: [MatDividerModule],
-  templateUrl: './terminos-condiciones.component.html',
-  styleUrl: './terminos-condiciones.component.scss',
+  selector: "app-terminos-condiciones",
+  imports: [MatDividerModule, MatButton, RouterLink, MatIconModule],
+  templateUrl: "./terminos-condiciones.component.html",
+  styleUrl: "./terminos-condiciones.component.scss",
 })
-export class TerminosCondicionesComponent { }
+export class TerminosCondicionesComponent {
+  showBack = inject(ActivatedRoute).snapshot.data["backButton"] || false;
+}
